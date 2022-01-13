27829ce5d4971f65963c35d9d3de8ff046081fe6
Andre Landim <andrelandim_4@hotmail.com>
2022-01-13 15:40:42 -0300

    Find movies

--- backend/src/main/java/com/devsuperior/dsmovie/dto/MovieDTO.java
+++ backend/src/main/java/com/devsuperior/dsmovie/dto/MovieDTO.java
@@ -1,32 +1,32 @@
-package com.devsuperior.dsmovie.entities;
+package com.devsuperior.dsmovie.dto;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import com.devsuperior.dsmovie.entities.Movie;
 
-@Entity
-@Table(name = "tb_movie")
-public class Movie {
+public class MovieDTO {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String title;
 	private Double score;
 	private Integer count;
 	private String image;
 	
-	public Movie() {
+	public MovieDTO() {
 	}
 	
-	public Movie(Long id, String title, Double score, Integer count, String image) {
+	public MovieDTO(Long id, String title, Double score, Integer count, String image) {
 		this.id = id;
 		this.title = title;
 		this.score = score;
 		this.count = count;
 		this.image = image;
+	}
+	
+	public MovieDTO(Movie entity) {
+		id = entity.getId();
+		title = entity.getTitle();
+		score = entity.getScore();
+		count = entity.getCount();
+		image = entity.getImage();
 	}
 	
 	public Long getId() {
@@ -59,5 +59,4 @@ public class Movie {
 	public void setImage(String image) {
 		this.image = image;
 	}
-	
 }
